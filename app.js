6a0e6d8b47795797eda7bba8ac3fd6800ff82d5d
Rod Boev <rod.boev@gmail.com>
2021-08-07 13:42:31 -0400

    Fix owner last name field

--- app.js
+++ app.js
@@ -41,10 +41,10 @@ for (var i in violations) {
 
 	for (var j in permits) {
 		if (violations[i].bin == permits[j].bin__) {
-			obj.bin = permits[j].bin__;
+			obj.bin = permits[j].bin__; 
 			obj.filing_date = permits[j].filing_date,
 			obj.owner_business = permits[j].owner_s_business_name,
-			obj.owner_name = `${permits[j].owner_s_first_name} ${permits[j].owner_s_owners_last_name}`,
+			obj.owner_name = `${permits[j].owner_s_first_name} ${permits[j].owner_s_owner_s_last_name}`,
 			obj.owner_address = `${permits[j].owner_s_house__} ${permits[j].owner_s_house_street_name}, ${permits[j].city}, ${permits[j].state}, ${permits[j].owner_s_zip_code}`,
 			obj.owner_phone = permits[j].owner_s_phone__
 		}
