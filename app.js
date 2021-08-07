82ccad8cebaadac96c00bc85f396253c125713f3
Rod Boev <rod.boev@gmail.com>
2021-08-07 16:28:12 -0400

    Build list of BINs for permit data request programmatically

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 var axios = require('axios');
 
-let violationsURL = "https://data.cityofnewyork.us/resource/mkgf-zjhb.json?$select=violationid,inspectiondate,novdescription,bin&$order=violationid%20DESC&$limit=10000"
-let permitsURL = "https://data.cityofnewyork.us/resource/ipu4-2q9a.json?$select=bin__,filing_date,owner_s_business_name,owner_s_first_name,owner_s_last_name,owner_s_house__,owner_s_house_street_name,city,state,owner_s_zip_code,owner_s_phone__&$order=filing_date%20DESC&$limit=1000"
- 
-const requestOne = axios.get(violationsURL);
-const requestTwo = axios.get(permitsURL);
+(async () => {
+	const violationsURL = "https://data.cityofnewyork.us/resource/mkgf-zjhb.json?$select=violationid,inspectiondate,novdescription,bin&$order=violationid%20DESC&$limit=50";
+	const violationsReq = await axios.get(violationsURL);
+	const violations = violationsReq.data;
 
-axios.all([requestOne, requestTwo]).then(axios.spread((...responses) => {	
-	const violations = responses[0].data;
-	const permits = responses[1].data;
+	let binSet = new Set();
+
+	for (let i in violations) {
+		binSet.add(violations[i].bin);
+	}
+
+	const binsToRequest = `('${Array.from(binSet).join("','")}')`;
+	const permitsURL = "https://data.cityofnewyork.us/resource/ipu4-2q9a.json?$select=bin__,filing_date,owner_s_business_name,owner_s_first_name,owner_s_last_name,owner_s_house__,owner_s_house_street_name,city,state,owner_s_zip_code,owner_s_phone__&$where=bin__%20in" + binsToRequest;
+
+	const permitsReq = await axios.get(permitsURL);
+	const permits = permitsReq.data;
 
 	let arrayList = [];
 
@@ -20,17 +27,18 @@ axios.all([requestOne, requestTwo]).then(axios.spread((...responses) => {
 				obj.inspectiondate = violations[i].inspectiondate;
 				obj.violation = violations[i].novdescription;
 				obj.bin = violations[i].bin;
+
 				obj.filed = permits[j].filing_date;
 				obj.owner_business = permits[j].owner_s_business_name;
 				obj.owner_name = `${permits[j].owner_s_first_name} ${permits[j].owner_s_last_name}`;
 				obj.owner_address = `${permits[j].owner_s_house__} ${permits[j].owner_s_house_street_name}, ${permits[j].city}, ${permits[j].state}, ${permits[j].owner_s_zip_code}`;
 				obj.owner_phone = permits[j].owner_s_phone__;
+
 				arrayList.push(obj);
 			}
 		}
 	}
 	
 	console.log(arrayList);
-})).catch(error => {
-	console.log(error);
-});
+
+})();
